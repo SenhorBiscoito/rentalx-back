7cd20a4082b875e6d546c5388ea43d35e53cf195
SenhorBiscoito <mirandajean2009@gmail.com>
2022-02-11 00:20:03 -0300

    feature: adicionando stream para ler arquivos csv

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,6 +1,17 @@
+import { Parser } from "csv-parse";
+import fs from "fs";
+
 class ImportCategoryUseCase {
   execute(file: Express.Multer.File): void {
-    console.log(file);
+    const stream = fs.createReadStream(file.path);
+
+    const parseFile = new Parser({});
+    stream.pipe(parseFile); // pipe the data to the parser
+
+    // when the parser emits a line
+    parseFile.on("data", async (line) => {
+      console.log(line);
+    });
   }
 }
 
