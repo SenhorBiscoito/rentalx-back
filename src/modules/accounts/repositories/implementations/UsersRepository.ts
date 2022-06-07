10fc6a0617a465110defb2c703a79221b087ed2e
Senhor Biscoito <mirandajean2009@gmail.com>
2022-06-07 18:35:59 -0300

    Criptografando senha

--- src/modules/accounts/repositories/implementations/UsersRepository.ts
+++ src/modules/accounts/repositories/implementations/UsersRepository.ts
@@ -26,6 +26,14 @@ class UsersRepository implements IUsersRepository {
 
     await this.repository.save(user);
   }
+
+  public async findByEmail(email: string): Promise<User | undefined> {
+    const user = await this.repository.findOne({
+      where: { email },
+    });
+
+    return user;
+  }
 }
 
 export { UsersRepository };
