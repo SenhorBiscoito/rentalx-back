c88dd3b554e4c67882b68c9e487b5997d2a2afe1
Senhor Biscoito <mirandajean2009@gmail.com>
2022-06-09 19:08:37 -0300

    Instalando & configurando jest

---
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -0,0 +1,11 @@
+describe("Soma de 2 + 2", () => {
+  it("espero que 2 + 2 seja 4", () => {
+    const soma = 2 + 2;
+    const resultado = 4;
+    expect(soma).toBe(resultado);
+  });
+
+  // it("deve criar uma categoria", async () => {
+
+  // });
+});
