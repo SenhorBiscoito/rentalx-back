8b9f1763e6f03d27ba1f2cb98fd9a3e1e8aaf2d8
Senhor Biscoito <mirandajean2009@gmail.com>
2022-06-10 09:18:25 -0300

    Refatorando a aplicação

--- src/shared/infra/middlewares/ensureAuthenticated.ts
+++ src/shared/infra/middlewares/ensureAuthenticated.ts
@@ -2,7 +2,8 @@ import { NextFunction, Request, Response } from "express";
 import { verify } from "jsonwebtoken";
 
 import { UsersRepository } from "@modules/accounts/infra/typeorm/repositories/UsersRepository";
-import { AppError } from "@shared/errors/AppError";
+
+import { AppError } from "../../errors/AppError";
 
 interface IPayload {
   sub: string;
